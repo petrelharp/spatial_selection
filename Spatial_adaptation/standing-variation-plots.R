815185434984fbc6e09f4ca513dd2a1ae8952140
Peter <Peter@ibis.ucdavis.edu/mw>
2011-04-07 22:15:33 +0000

    /* Plots */

--- Spatial_adaptation/standing-variation-plots.R
+++ Spatial_adaptation/standing-variation-plots.R
@@ -9,8 +9,8 @@ sd <- .01
 sigmavals <- c(0.1,1,10,20,50)
 
 # plot Standing Proportion Numbers as a function of sigma
-sPvals <- sapply(sigmavals, function (sigma) { standingProportionNumbers(mu,rho,sb,sd,sigma)$value } )
-plot(sigmavals, sPvals, type="b", xlab=expression(paste(sigma, " = dispersal SD")), ylab=expression(paste(z[0], " = proportion of patches from standing variation")), ylim=c(0,1))
+# sPvals <- sapply(sigmavals, function (sigma) { standingProportionNumbers(mu,rho,sb,sd,sigma)$value } )
+# plot(sigmavals, sPvals, type="b", xlab=expression(paste(sigma, " = dispersal SD")), ylab=expression(paste(z[0], " = proportion of patches from standing variation")), ylim=c(0,1))
 
 
 # sickle cell case from ralphcoop2010
@@ -25,7 +25,7 @@ otherlabs <- expression(paste( mu == 10^-8, "  ", s[b] == .05, "  ", s[d] == .01
 
 # plot Standing Proportion Numbers as a function of sigma
 setEPS(horizontal = FALSE, onefile = FALSE, paper = "special")
-postscript( file="proportion-by-sigma.eps", width=6.5, height=3, title=paramString() )
+postscript( file="proportion-by-sigma.eps", width=6.5, height=5, title=paramString() )
 plot(0, 0, type="n", xlab=expression(paste(sigma, " = dispersal SD")), ylab=expression(paste(z[0], " = proportion of patches from standing variation")), xlim=range(sigmavals), ylim=c(0,1), main=otherlabs )
 legend("topleft", lty=1, col=rhocols, legend=rholabs)
 for (k in 1:length(rhovals)) {
