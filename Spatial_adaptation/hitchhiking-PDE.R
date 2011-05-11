6c9f7a2be7a6c46d2e09b08935fdac494c4da9cc
Peter <Peter@ibis.ucdavis.edu/mw>
2011-05-11 20:37:11 +0000

    WorkingWiki importation

--- Spatial_adaptation/hitchhiking-PDE.R
+++ Spatial_adaptation/hitchhiking-PDE.R
@@ -40,7 +40,7 @@ phiEst <- lsoda(y=yinit, times=tt, func=stationaryPhi, parms=list(v=vEst))
 dynamicPhi <- function(t,y,parms,xx,dx) {
     # solving the PDE
     # and waiting for stationarity
-    #   d/dt y = y'' + y (1-y)
+    #   d/dt y = v y' + y'' + y (1-y)
     #  here y is the vector phi(xx)
     #  and yp is the vector phi'(xx)
     n <- length(xx)
@@ -51,7 +51,7 @@ dynamicPhi <- function(t,y,parms,xx,dx) {
     # and f''(x) approx 2 * ( (f(x+a)-f(x))/a - (f(x)-f(x-b))/b ) / (a+b)
     dyp <- diff(yp)/dx
     ypp <- c(dyp[1], (dx[-1]*dyp[-(n-1)]+dx[-(n-1)]*dyp[-1])/(dx[-1]+dx[-(n-1)]) , dyp[n-1])
-    z <- -parms$v * yp + ypp + y*(1-y)
+    z <- parms$v * yp + ypp + y*(1-y)
     # if (any(abs(z)>10)) { browser() }
     return( list( z ) )
 }
@@ -60,9 +60,10 @@ xx <- (-1000:1000)/100
 dx <- diff(xx)
 yinit <- (1-tanh(xx/2))/2
 yinit <- sort(runif(length(xx)),decr=TRUE)
-yytt <- ode.1D(yinit,(1:100)/30,dynamicPhi,parms=list(v=sqrt(2)),nspec=1,dimens=length(yinit),xx=xx, dx=dx, maxsteps=50000 )
+v <- 4
+yytt <- ode.1D(yinit,(1:100)/20,dynamicPhi,parms=list(v=v),nspec=1,dimens=length(yinit),xx=xx, dx=dx, maxsteps=50000, atol=0 )
 
-speed <- 3
+speed <- 0
 plot( xx-yytt[1,1]*speed, yytt[1,-1], type="l" )
 for (k in 2:(dim(yytt)[1]-1)) { lines(xx-yytt[k,1]*speed,yytt[k,-1],col=rainbow(dim(yytt)[1]+5)[k]) }
 
@@ -70,11 +71,51 @@ for (k in 2:(dim(yytt)[1]-1)) { lines(xx-yytt[k,1]*speed,yytt[k,-1],col=rainbow(
 ### useful for debugging
 ggg <- function () {
    with( parent.frame(), {
-     ylims <- range(c(y,yp,ypp)); plot(xx[-1],y,ylim=ylims)
-     points(xx[-1],yp,col="red")
-     points(xx[-1],ypp,col="green")
+     ylims <- range(c(y,yp,ypp)); plot(xx,y,ylim=ylims)
+     points(xx,yp,col="red")
+     points(xx,ypp,col="green")
    } )
 }
 trace(dynamicPhi,exit=ggg)
 
 
+### PROBLEMS ABOVE. with boundary conditions?
+
+logitPhi <- function(t,y,parms,xx,dx) {
+    # solving the PDE
+    # and waiting for stationarity
+    #   d/dt y = v y' + y'' + y (1-y)
+    #  here y is the vector phi(xx)
+    #  and yp is the vector phi'(xx)
+    ## Try putting through logit function:
+    # if df/dt = v f' + f'' + f(1-f)
+    # and y = log(f/(1-f))
+    # then dy/dt = v y' + (y')^2 + (y'')/(1+e^{-y}) + 1
+    n <- length(xx)
+    # dx <- diff(xx)  # length n-1
+    dy <- diff(y)/dx  # length n-1
+    # f'(x) approx ( (b/a+b) (f(x+a)-f(x))/a + (a/a+b) (f(x)-f(x-b))/b )
+    yp <- c(dy[1], (dx[-1]*dy[-(n-1)]+dx[-(n-1)]*dy[-1])/(dx[-1]+dx[-(n-1)]) , dy[n-1])  # length n
+    # and f''(x) approx 2 * ( (f(x+a)-f(x))/a - (f(x)-f(x-b))/b ) / (a+b)
+    dyp <- diff(yp)/dx
+    ypp <- c(dyp[1], (dx[-1]*dyp[-(n-1)]+dx[-(n-1)]*dyp[-1])/(dx[-1]+dx[-(n-1)]) , dyp[n-1])
+    z <- parms$v * yp + yp^2 + ypp/(1+exp(-y)) + 1
+    # if (any(abs(z)>10)) { browser() }
+    return( list( z ) )
+}
+
+logit <- function (x) { log(x/(1-x)) }
+logist <- function (x) { 1 / (1 + exp(-x)) }
+
+xx <- (-1000:1000)/100
+dx <- diff(xx)
+finit <- (1-tanh(xx/2))/2
+# OR
+finit <- sort(runif(length(xx)),decr=TRUE)
+yinit <- logit(finit)
+v <- 4
+yytt <- ode.1D(yinit,(1:100)/20,dynamicPhi,parms=list(v=v),nspec=1,dimens=length(yinit),xx=xx, dx=dx, maxsteps=50000 )
+
+speed <- 0
+plot( xx-yytt[1,1]*speed, logist(yytt[1,-1]), type="l" )
+for (k in 2:(dim(yytt)[1]-1)) { lines(xx-yytt[k,1]*speed, logist(yytt[k,-1]), col=rainbow(dim(yytt)[1]+5)[k]) }
