6950045bf297b466f2a9c15397bd446115961c5a
Peter <Peter@ibis.ucdavis.edu/mw>
2011-05-17 18:01:41 +0000

    WorkingWiki importation

--- sim-lineages.R
+++ sim-lineages.R
@@ -8,15 +8,15 @@ m <- 0.05
 N <- 1000
 npops <- 100
 ntypes <- 2
-migrsteps <- lapply( 1:10, function (n) { c( 2^(-(n+2)), n, 0 ) } )
-migrsteps <- c( migrsteps, lapply( 1:10, function (n) { c( 2^(-(n+2)), -n, 0 ) } ) )
+migrsteps <- lapply( 1:5, function (n) { c( 2^(-(n+2)), n, 0 ) } )
+migrsteps <- c( migrsteps, lapply( 1:5, function (n) { c( 2^(-(n+2)), -n, 0 ) } ) )
 
 # number of lineages
 nlin <- 1000
 # recomb rate
 recomb <- 1e-3
 # number of time steps:
-nsteps <- 1000
+nsteps <- 2000
 
 # start near the edge
 wavestart <- 1
@@ -25,9 +25,10 @@ initpop$n[1,,2] <- 0  # put an initial population where we want it
 initpop$n[1,wavestart,2] <- N
 initpop$n[1,wavestart,1] <- N-initpop$n[1,wavestart,2] 
 
-# after has run once already:
-if (FALSE) {
-    initpop$n <- pophist$pophist[,,,100]
+# get good initial values
+if (TRUE) {
+    pophist <- pophistory( pop=initpop, nsteps=2, step=300 )
+    initpop$n <- pophist$pophist[,,,2]
     dim(initpop$n) <- dim(pophist$pophist)[1:3]
 }
 
@@ -35,15 +36,8 @@ if (FALSE) {
 pophist <- pophistory( pop=initpop, nsteps=nsteps, step=1)
 lins <- lineages( pophist$pophist, nlin=nlin, migr=migrsteps, T=nsteps, m=m )
 
-# plot time-by-space in one dimension
-zimg <- plotPop( aperm(pophist$pophist[1,,,],c(3,1,2)), xlab="time", ylab="space" )
-axis(1); axis(2)
-# add lineages
-cols <- rainbow(nlin)
-for (k in 1:nlin) {
-    z <- lins$lineagelocs[,,k]
-    lines( dim(z)[2]:1, z["x",], col=cols[k], lwd=2 )
-}
+# save??
+save(pophist,initpop,lins,recomb,migrsteps,nlin,nsteps,file="lineage-sims.RData")
 
 # compute functions of the path
 lins$recomb <- apply(lins$localsizes, 3, function (x) { cumsum( recomb*(1-x/N) ) })
@@ -59,8 +53,20 @@ iord <- order(initdist)  # use this to order by initial distance
 #   = P{ at least one recomb } * global het
 global.het <- 0.82
 recombvals <- (1:20)/5 * recomb
-hetprobs <- sapply(recombvals/recomb, function (relrecomb) { 1-exp( - relrecomb*lins$recomb ) } )
-dim(hetprobs) <- c(dim(lins$recomb),length(recombvals))
+hetprobs <- sapply(recombvals/recomb, function (relrecomb) { global.het*(1-exp( - relrecomb*lins$recomb[nsteps,] )) } )
+
+# plot time-by-space in one dimension
+zimg <- plotPop( aperm(pophist$pophist[1,,,],c(3,1,2)), xlab="time", ylab="space" )
+axis(1); axis(2)
+# add lineages
+# for just lineages do:
+plot(0,type="n",xlim=c(0,nsteps),ylim=c(0,npops),xlab="time",ylab="space")
+cols <- rainbow(nlin, alpha=min(1,40/nlin))
+fullcols <- rainbow(nlin)
+for (k in 1:nlin) {
+    z <- lins$lineagelocs[,,iord[k]]
+    lines( dim(z)[2]:1, z["x",], col=cols[k], lwd=2 )
+}
 
 plot(0,ylim=range(lins$localsizes),xlim=c(0,nsteps),type="n", xlab="time", ylab="local population size")
 for (k in 1:nlin) { lines(rev(lins$localsizes[1,,k]), col=cols[k]) }
@@ -69,7 +75,7 @@ plot(0,ylim=range(lins$recomb),xlim=c(0,nsteps),type="n")
 for (k in 1:nlin) { lines( rev(lins$recomb[,k]), col=cols[k]) }
 
 plot(0,ylim=range(lins$coal),xlim=c(0,nsteps),type="n")
-for (k in 1:nlin) { lines( rev(lins$coal[,k]), col=cols[k]) }
+for (k in 1:nlin) { lines( rev(lins$coal[,k]), col=fullcols[k]) }
 
 # plot the distrn of the prob of recombination
 plot(lins$precomb["Mean",], type="l")
@@ -77,3 +83,36 @@ lines(lins$precomb["1st Qu.",], lty=2)
 lines(lins$precomb["3rd Qu.",], lty=2)
 lines(lins$precomb["Min.",], lty=3)
 lines(lins$precomb["Max.",], lty=3)
+
+# plot heterozygosities
+# as a function of geographic distance
+plot( initdist[iord], hetprobs[iord,1], type="n", ylim=c(0,1), xlab="distance from origin", ylab="heterozygosity" )
+legend("topleft", col=c(NA,fullcols), lty=c(NA,1), legend=c("recombination",recombvals) )
+for (k in 1:dim(hetprobs)[2]) {
+    points(initdist[iord], hetprobs[iord,k], col=cols[k])
+    lines( lowess(initdist[iord], hetprobs[iord,k], f=.3), col=fullcols[k] )
+}
+
+# ... and as a function of geographic distance
+binlocs <- pretty(initdist,n=5)
+nbins <- length(binlocs)-1
+binmids <- (binlocs[-(nbins+1)]+binlocs[-1])/2
+bincols <- rainbow(2*nbins)[1:nbins]
+plot( recombvals, hetprobs[1,], type="n", xlim=c(0,max(recombvals)), ylim=c(0,1), xlab="map distance", ylab="heterozygosity" )
+legend("topleft", legend=binmids, col=bincols, lty=1, title="Distance to origin", bg="white")
+abline(h=global.het, lty=2)
+for ( ell in 1:nbins ) {
+    iii <- (initdist >= binlocs[ell]) & (initdist < binlocs[ell+1])
+    #    lll <- lowess(rep(recombvals,each=sum(iii)), hetprobs[iii,], f=.3)
+    #    lines( lll, col=bincols[ell], lwd=2 )
+    #    means <- lll$y[match(recombvals,lll$x)]
+    rvals <- jitter(recombvals)
+    summ <- sapply(1:length(recombvals), function (x) summary(hetprobs[iii,x]) ) 
+    lines( recombvals, summ["Mean",], lwd=2, col=bincols[ell] )
+    arrows( x0=rvals, y0=summ["1st Qu.",], y1=summ["3rd Qu.",], angle=90, length=0, col=bincols[ell] )
+    arrows( x0=rvals, y0=summ["1st Qu.",], y1=summ["3rd Qu.",], angle=90, length=0, col=bincols[ell] )
+    for (k in (1:nlin)[iii]) {
+        points(jitter(recombvals), hetprobs[k,], col=cols[k], pch=20, cex=0.5)
+    }
+}
+
