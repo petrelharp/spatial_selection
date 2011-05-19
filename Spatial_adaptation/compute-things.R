7d2d3933be1accf7c02d00a0364ab8d09bf2a6d5
Peter <Peter@ibis.ucdavis.edu/mw>
2011-05-19 06:59:52 +0000

    /* Code to do the computations */ changed sigma to 50...

--- Spatial_adaptation/compute-things.R
+++ Spatial_adaptation/compute-things.R
@@ -2,41 +2,59 @@ source("Spatial_adaptation/standing-variation-fns.R")
 require(xtable)
 
 examples <- list( 
-    "HbS" = list(
+    "HbS0" = list(
         mu = 10^(-8),
-        rho = 2,
+        rho = 2.5,
         xi = 2,
         # sb = .05,  # this is what we had before?
         sb = .15,  # but it seems to actually be this (currat et al 2002, hedrick review)
         sd = .1,  # ???
-        sigma = 10,
+        sigma = 10,  # what we had before
+        R = 4000
+    ),
+    "HbS weak" = list(
+        mu = 10^(-8),
+        rho = 2.5,
+        xi = 2,
+        sb = .15,
+        sd = .1,  # ???
+        sigma = 50,  # closer to estimated value of 100?  gets right char length
+        R = 4000
+    ),
+    "HbS strong" = list(
+        mu = 10^(-8),
+        rho = 2.5,
+        xi = 2,
+        sb = .15,  
+        sd = .5,  # doesn't matter much
+        sigma = 50,
         R = 4000
     ),
     "HbC" = list(
         mu = 10^(-8),
-        rho = 2,
+        rho = 2.5,
         xi = 2,
         sb = .07,  # Wood et al 2005 (.04-.09)
         sd = .1,  # ???
-        sigma = 10,
+        sigma = 50,
         R = 4000
     ),
     "G6PD strong" = list(
         mu = 150*10^(-8),  # 150 bp
-        rho = 2*0.75,  # on the X
-        xi = 2,
+        rho = 2.5*0.75,  # on the X
+        xi = 2.5,
         sb = .25,  # Slatkin et al 2008
         sd = .1,
-        sigma = 10,
+        sigma = 50,
         R = 4000
     ),
     "G6PD weak" = list(
         mu = 150*10^(-8),  # 150 bp
-        rho = 2*0.75,  # on the X
+        rho = 2.5*0.75,  # on the X
         xi = 2,
         sb = .04,  # Tiskkoff et al 2001
         sd = .1,
-        sigma = 10,
+        sigma = 50,
         R = 4000
     )
 )
