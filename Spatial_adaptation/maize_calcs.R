7621523845b747b2db77f744436545e947914713
Peter <Peter@ibis.ucdavis.edu/mw>
2011-06-09 04:56:14 +0000

    /* Maize */ numbers update

--- Spatial_adaptation/maize_calcs.R
+++ Spatial_adaptation/maize_calcs.R
@@ -1,14 +1,22 @@
 
-# estimated
+# From Jeff:
+N <- 103359
+Nf <- 561
+m <- 0.2
+e <- 0.068
+mg <- .0083
+pm <- .02
+n <- 325
+
+# from their paper
 N <- 100000
 Nf <- 500
 m <- 0.3
 e <- 0.1
 mg <- .018
-pm <- .01
+pm <- .02
 # number of demes = ???
-# n <- 10000  # gets Fst = .011
-n <- 28  # gets Fst = .008
+n <- 325  # gets Fst = .011
 
 
 # calculated
@@ -38,13 +46,31 @@ T1 <- T0 * (1-Q*P4) + 1/sum(b)
 Fst <- (1-1/n) / ( (1-1/n) + sum(b) * T0 )
 
 ## Results:
-# with n = 10000
-# T0 = 3690781
-# T1 = 3734148
-# Fst = 0.01161358
+# Jeff's parameters:
+# T0 = 134640
+# T1 = 136724
+# Fst = 0.01519433
 
-# with n = 28
-# T0 = 13858
-# T1 = 13975
-# Fst = 0.008052662
+# paper's parameters:
+# T0 = 140435
+# T1 = 141809
+# Fst = 0.009655918
+
+
+###
+# Branching process
+# probability $$(1/2)$$: mean (1+s) (pollen)
+# probability $$(1/2) (1-N_f/N) = .4975$$: 0 (not seed corn)
+# probability $$(1/2) N_f/N = .0025$$: $$(1+s)N/Nf=200(1+s)$$ (seed corn)
+#
+# note mean is s
+#  and variance is (1/2)*(1+s)( 1 + (1+s)) + (1/2)*N_f/N* (1+s) (N/Nf) (1 + (1+s) N/Nf ) - s^2
+#                 = (1/2)(1+s)(2+s) - s^2 + (1/2) (1+s) (1+(1+s)(N/Nf)) , i.e. approximately N/2Nf = 100.
+
+s <- .05
+# mixture of Poissons:
+genfn <- function (x) { (1/2) * ( exp((1+s)*(x-1)) + (1-Nf/N) + Nf/N * exp((x-1)*(1+s)*N/Nf) ) }
+# Find the root of genfn(x)-x=0 :
+# solution is approx 0.9999 , i.e. 1-2*s/100.
+
 
