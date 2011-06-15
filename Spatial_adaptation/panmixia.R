0291fa15b5d26d1a48b905e8213c6831ed635331
Peter <Peter@ibis.ucdavis.edu/mw>
2011-06-15 20:15:16 +0000

    *Empty MediaWiki Message*

--- Spatial_adaptation/panmixia.R
+++ Spatial_adaptation/panmixia.R
@@ -39,7 +39,7 @@ dim(ddd)<-c(length(ss),length(rr))
 # contour(x=ss,y=rr,z=ddd,xlab="s",ylab="r")
 
 # png(file="min-s-versus-r.png",width=600,height=400)
-pdf(file="min-s-versus-r.pdf",width=6,height=4)
+pdf(file="Spatial_adaptation/min-s-versus-r.pdf",width=6,height=4)
 rr <- 10:1000
 plot(rr,8*((log(rr)/rr)^2), type="l", xlab=expression(r==sqrt(A/sigma^2)), ylab=expression(s== 8 * ( log(r)/r )^2), log="xy")
 dev.off()
@@ -48,7 +48,7 @@ NN <- exp(8:20)
 plot(NN, 1/(4*NN*log(NN)), type="l")
 
 # png(file="f-versus-gamma.png",width=600,height=400)
-pdf(file="f-versus-gamma.pdf",width=6,height=4)
+pdf(file="Spatial_adaptation/f-versus-gamma.pdf",width=6,height=4)
 xx <- (1:999)/1000
 plot((1-xx^2)/xx^3, xx, type="l", log="xy", xlab=expression(gamma==(1-x)/x^(3/2)), ylab=expression(x==(chi/chi[0])^2) )
 dev.off()
@@ -60,7 +60,7 @@ sigsig <- (10:100)
 gg <- apply( expand.grid(ss,sigsig), 1, function (ssig) log( sqrt(ssig[1]) / ( ssig[2] * sqrt(4*pi*mu) * log(ssig[2]^2)^(3/2) ) ) )
 dim(gg) <- c(length(ss), length(sigsig))
 # png(file="gamma-contour-1e-8.png", width=600, height=400)
-pdf(file="gamma-contour-1e-8.pdf", width=6, height=4)
+pdf(file="Spatial_adaptation/gamma-contour-1e-8.pdf", width=6, height=4)
 contour(x=ss,y=sigsig,z=gg, xlab="s", ylab=expression(sigma), main=expression(paste(log(gamma),", ", mu==10^-8)))
 dev.off()
 
