61a27fd70e3f6f6933fbd5c48a9bef27184850a0
Peter <Peter@ibis.ucdavis.edu/mw>
2011-06-29 04:53:57 +0000

    WorkingWiki replaced project description

---
+++ Spatial_adaptation/patchy-selection-plots.R
@@ -0,0 +1,15 @@
+
+source("Spatial_adaptation/patchy-selection-fns.R")
+
+# how big to make the figures
+figwidth <- 10
+figheight <- 8
+
+# playing around
+mu <- 10^(-8)
+rho <- 10
+sb <- .01
+sd <- .01
+sigmavals <- c(0.1,1,10,20,50)
+
+
