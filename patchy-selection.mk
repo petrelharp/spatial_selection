785c7ee7cc590f9d1b4aa303762fb2bd0608078e
Peter <Peter@ibis.ucdavis.edu/mw>
2011-07-01 12:58:49 +0000

    /* Makefile */

--- patchy-selection.mk
+++ patchy-selection.mk
@@ -1,6 +1,6 @@
 
-# plots = helianthus-ex-table.tex helianthus-standing-ex-table.tex
+plots = helianthus-ex-table.tex helianthus-standing-ex-table.tex
 
 $(plots): Spatial_adaptation/patchy-selection-plots.R Spatial_adaptation/patchy-selection-fns.R
-	nice -19 R --vanilla < $< 
+#	nice -19 R --vanilla < $< 
 
