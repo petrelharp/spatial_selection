8cb1316e60633990aeebfc732d372c3be83b6885
Peter <Peter@ibis.ucdavis.edu/mw>
2013-07-23 19:59:07 +0000

    WorkingWiki replaced project description

--- sim-patchy-selection.R
+++ sim-patchy-selection.R
@@ -1,8 +1,15 @@
 #!/usr/bin/R
 source("sim-patchy-selection-fns.R")
 source("lineages.R")
+source("sim-patchy-selection-fns.R")
 
 run.list <- list.files(".","*-pophistory-run.Rdata")
+if (!file.exists("run-info.csv")) {
+    rundims <- sapply( run.list, function (x) { tmp <- load(x); if ('pophist' %in% tmp) { dim(pophist$pophist) } else { NA } } )
+    write.table(rundims,file="run-info.csv",sep=',')
+} else {
+    rundims <- read.csv("run-info.csv")
+}
 
 # load random run
 rm(pophist); rm(lins)
@@ -43,17 +50,20 @@ plotlins(lins,add=TRUE,subset=1:100)
 
 # Exponential decay in mean occupation density
 plot( pophist$pop$params$patchdist, rowMeans(pophist$pophist[,,2,200:dim(pophist$pophist)[4],drop=FALSE],dims=2)/pophist$pop$params$N, log='y', col=adjustcolor("black",.25), pch=20 )
-with(pophist$pop$params, lines( seq(0,max(pophist$pop$params$patchdist),length.out=100), 1.5*exp(-(sqrt(2*abs(sm))/getsigma(pophist$pop$params))*seq(0,max(pophist$pop$params$patchdist),length.out=100)) ) )
-with(pophist$pop$params, lines( seq(0,max(pophist$pop$params$patchdist),length.out=100), 1.5*exp(-2*(sqrt(2*abs(sm))/getsigma(pophist$pop$params))*seq(0,max(pophist$pop$params$patchdist),length.out=100)), lty=2 ) )  # fudge factor of 2
-
+with(pophist$pop$params, 
+    lines( seq(0,max(pophist$pop$params$patchdist),length.out=100), 
+        1.5*exp(-(sqrt(2*abs(sm))/getsigma(pophist$pop$params))*seq(0,max(pophist$pop$params$patchdist),length.out=100)) ) )
+with(pophist$pop$params, 
+    lines( seq(0,max(pophist$pop$params$patchdist),length.out=100), 
+        1.5*exp(-2*(sqrt(2*abs(sm))/getsigma(pophist$pop$params))*seq(0,max(pophist$pop$params$patchdist),length.out=100)), lty=2 ) )  # fudge factor of 2 
 
 # Look at occupation times of farout stuff
-plot.times <- (2*pophist$pop$params$patchsize):(4*pophist$pop$params$patchsize)
-matplot( pophist$pophist[,plot.times,2,], type='l', col=rainbow(length(plot.times)), lty=1 )
-legend("topright",plot.times,lty=1,col=rainbow(length(plot.times)),legend=plot.times)
+plot.times <- (4*pophist$pop$params$patchsize):(5*pophist$pop$params$patchsize)
+matplot( t(pophist$pophist[,plot.times,2,]), type='l', col=rainbow(length(plot.times)), lty=1, xlab='time', ylab='density' )
+legend("topright",plot.times,lty=1,col=rainbow(length(plot.times)),legend=plot.times,title='space')
 
 matplot( pophist$pop$params$patchdist, pophist$pophist[,,2,], type='l', col=adjustcolor(rainbow(dim(pophist$pophist)[4]),.25), lty=1, ylim=c(0,50) )
-abline(v=params$patchsize,lwd=2)
+with(pophist$pop, abline(v=params$patchsize,lwd=2) )
 
 ######
 # plot lineages, 1D
@@ -66,7 +76,7 @@ plotpop( aperm(pophist$pophist[1,,,],c(3,1,2)), params=pophist$pop$params, xlab=
 axis(1); axis(2)
 # plot(0,type="n",xlim=c(0,nlin),ylim=c(0,max(dim(lins$lineagelocs)[1:2])),xlab="time",ylab="space")
 cols <- rainbow(dim(lins$lineagelocs)[3], 0.1+.7*(initdist/max(initdist)))
-matplot( jitter(lins$lineagelocs["x",,]), col=cols, type='l' )
+matlines( jitter(lins$lineagelocs["x",,]), col=cols, type='l' )
 
 
 #######
