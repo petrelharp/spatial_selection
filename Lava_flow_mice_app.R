b5775a88d86c5daebe86db99ac935590a4b92726
peter <petrel.harp@gmail.com>
2014-07-02 09:58:37 -0700

    peter edited through discussion

--- Lava_flow_mice_app.R
+++ Lava_flow_mice_app.R
@@ -11,8 +11,8 @@ R = seq(5,600,length=10000) ##distance in Km
 #layout(t(1:2))
 mu = 1e-8
 prob.parallel <- mu*A/(mu*A + (1/char.length[1])* exp(-R/char.length[1]))  ##assuming sigma=1
-plot(R, prob.parallel,type="l",lwd=3, xlab="", ylab="",cex.lab=1.3,cex.axis=1.3,log="x") 
-mtext(side=2,line=2.5,"prob parallel mutation",cex=1.3)
+plot(R, prob.parallel,type="l",lwd=3, xlab="", ylab="",log="x") 
+mtext(side=2,line=2.5,"prob parallel mutation")
 mu=1e-5
 prob.parallel <- mu*A/(mu*A + (1/char.length[1])* exp(-R/char.length[1]))  ##assuming sigma=1
 
@@ -31,16 +31,16 @@ lines(R, prob.parallel,lwd=3,col="red",lty=2)
 
 legend("topleft", legend=expression(mu == 10^-8, mu == 10^-5, sigma/sqrt(s[m]) == 3,sigma/sqrt(s[m]) == 30),col=c("black","red","grey","grey"),lty=c(1,1,1,2),lwd=2)
 
-mtext(side=1,line=2.5,"Distance between patches (km)",cex=1.3)
+mtext(side=1,line=2.5,"Distance between patches (km)")
 
 
 ####haplotype lengths
 ##assuming sigma=1
 length.hap.1<-sqrt(2)*1/(char.length[1])/R +1/R^2
 length.hap.2<-sqrt(2)*1/(char.length[2])/R +1/R^2
-plot(R,length.hap.1*100,type="l",log="x",ylab="", xlab="",lty=1,lwd=2,cex.lab=1.3,cex.axis=1.3)
-mtext(side=2,line=2.5,"Shared Haplotype Length (cM)",cex=1.3)
-mtext(side=1,line=2.5,"Distance between patches (km)",cex=1.3)
+plot(R,length.hap.1*100,type="l",log="x",ylab="", xlab="",lty=1,lwd=2)
+mtext(side=2,line=2.5,"shared haplotype length (cM)")
+mtext(side=1,line=2.5,"distance between patches (km)")
 lines(R,length.hap.2*100,lty=2,lwd=2)
 
 legend("topright", legend=expression( sigma/sqrt(s[m]) == 3,sigma/sqrt(s[m]) == 30),col=c("grey","grey"),lty=c(1,2),lwd=2)
