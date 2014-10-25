54a0c7031ba419c43ce0ca3e4502308402e07612
peter <petrel.harp@gmail.com>
2014-10-25 12:02:08 -0700

    .

--- collate-sims.R
+++ collate-sims.R
@@ -25,12 +25,13 @@ if (ncol(simfiles)==5) {
 sim.params <- lapply( file.path(basedir,raw.simfiles), function (x) {
             load(x)
             atime.vals <- rep(NA, 3*3)
-            names(atime.vals) <- c( paste("final",0:2,sep=''), paste("time",0:2,sep=''), paste("size",0:2,sep='')  )
+            names(atime.vals) <- c( paste("final",0:2,sep=''), paste("time",0:2,sep=''), paste("size",0:2,sep=''), paste("hit100.",0:2,sep='') )
             if (min(pophist$pop$params$range)==1) {
                 atimes <- adapttime.1D(pophist)
                 atime.vals[1:nrow(atimes)] <- atimes$final
                 atime.vals[3+(1:nrow(atimes))] <- atimes$time
                 atime.vals[2*3+(1:nrow(atimes))] <- atimes$size
+                atime.vals[3*3+(1:nrow(atimes))] <- atimes$hit100
             }
             c( pophist$pop$params[c("mu","r","m","N","range","ntypes","sb","sm","nsteps","stepsize","sigma")], atime.vals )
         } )
